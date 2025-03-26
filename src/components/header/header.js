07d2d2e3018808f4d0482ebf839bdb4427698de8
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-03-27 00:55:02 +0200

    start header

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+export const headerComponent = `
+  <header class="header__hero">
+  <div class="header__top">
+    <div class="header__loop"><img src="/images/svg/search.svg" alt="search"></div>
+    <h1 class="header__logo">Avion</h1>
+    <div class="header__user"></div>
+  </div>
+  <div class="header__line"></div>
+  <div class="header__bottom">
+    <ul class="header__list">
+      <a href="#" class="header__list__item"><li>Plant pots</li></a>
+      <a href="#" class="header__list__item"><li>Ceramics</li></a>
+      <a href="#" class="header__list__item"><li>Tables</li></a>
+      <a href="#" class="header__list__item"><li>Chairs</li></a>
+      <a href="#" class="header__list__item"><li>Crockery</li></a>
+      <a href="#" class="header__list__item"><li>Tableware</li></a>
+      <a href="#" class="header__list__item"><li>Cutlery</li></a>
+    </ul>
+  </div>
+</header>
+`
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import '../scss/style.scss';
 import { writePost, readPosts } from '../../data/firebase.js'; 
+import { headerComponent } from '../components/header/header.js';
 
+let app = document.getElementById('app');
+
+app.innerHTML = `${headerComponent}`;
