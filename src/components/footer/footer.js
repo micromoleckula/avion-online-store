d76f62409166867dc8556ed7a486d184a5055e6c
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-03-28 23:22:48 +0200

    start footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,52 @@
+export const footerComponent = `
+  <footer class="footer__hero">
+    <div class="footer__top">
+      <div class="footer__list__box">
+        <ul class="footer__list__menu">
+          <h5 class="footer__list-title">Menu</h5>
+          <li><a class="footer__list-item">New arrivals</a></li>
+          <li><a class="footer__list-item">Best sellers</a></li>
+          <li><a class="footer__list-item">Recently viewed</a></li>
+          <li><a class="footer__list-item">Popular this week</a></li>
+          <li><a class="footer__list-item">All products</a></li>
+        </ul>
+        <ul class="footer__list__categories">
+          <h5 class="footer__list-title">Categories</h5>
+          <li><a class="footer__list-item">Crockery</a></li>
+          <li><a class="footer__list-item">Furniture</a></li>
+          <li><a class="footer__list-item">Homeware</a></li>
+          <li><a class="footer__list-item">Plant pots</a></li>
+          <li><a class="footer__list-item">Chairs</a></li>
+          <li><a class="footer__list-item">Crockery</a></li>
+        </ul>
+        <ul class="footer__list__company">
+          <h5 class="footer__list-title">Our company</h5>
+          <li><a class="footer__list-item">About us</a></li>
+          <li><a class="footer__list-item">Vacancies</a></li>
+          <li><a class="footer__list-item">Contact us</a></li>
+          <li><a class="footer__list-item">Privacy</a></li>
+          <li><a class="footer__list-item">Returns policy</a></li>
+        </ul>
+      </div>
+      <div class="footer__email">
+        <p class="footer__email-paragraph">Join our mailing list</p>
+        <div class="footer__input-wrapper">
+          <input class="footer__input" type="email" placeholder="your@email.com">
+          <button class="footer__input-button">Sign up</button>
+        </div>
+      </div>
+    </div>
+    <div class="footer__line"></div>
+    <div class="footer__bottom">
+      <p class="footer__bootom__text">SRDP TEXH .inc</p>
+      <div class="footer__bottom__social">
+        <img src="/images/svg/logo--linkedin.svg">
+        <img src="/images/svg/logo--facebook.svg">
+        <img src="/images/svg/logo--instagram.svg">
+        <img src="/images/svg/logo--skype.svg">
+        <img src="/images/svg/logo--twitter.svg">
+        <img src="/images/svg/logo--pinterest.svg">
+      </div>
+    </div>
+  </footer>
+`
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import '../scss/style.scss';
 import { writePost, readPosts } from '../../data/firebase.js'; 
 import { headerComponent } from '../components/header/header.js';
+import { footerComponent } from '../components/footer/footer.js';
 
 let app = document.getElementById('app');
 
-app.innerHTML = `${headerComponent}`;
+app.innerHTML = `
+${headerComponent}
+${footerComponent}
+`;
