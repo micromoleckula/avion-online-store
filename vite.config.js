8540fbca347a5ab4f5bff9f59054c3e17032c8c5
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-04-05 23:40:35 +0300

    delete rollup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import html from '@rollup/plugin-html';
 
 export default defineConfig({
-  plugins: [html()],
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        addProduct: path.resolve(__dirname, 'public/add-products.html'),
-        allProducts: path.resolve(__dirname, 'public/all-products.html'),
-        basket: path.resolve(__dirname, 'public/basket.html'),
+        main: path.resolve(__dirname, '/public/index.html'),
+        addProduct: path.resolve(__dirname, '/public/add-products.html'),
+        allProducts: path.resolve(__dirname, '/public/all-products.html'),
+        basket: path.resolve(__dirname, '/public/basket.html'),
       },
     },
   },
