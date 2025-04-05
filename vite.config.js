d8067764c132bdd552197cb526b869993a7333fc
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-04-05 22:44:28 +0300

    path changes and verification

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        addProduct: path.resolve(__dirname, 'src/pages/add-products/add-products.html'),
-        allProducts: path.resolve(__dirname, 'src/pages/all-products/all-products.html'),
-        basket: path.resolve(__dirname, 'src/pages/basket/basket.html'),
+        addProduct: path.resolve(__dirname, '/add-products.html'),
+        allProducts: path.resolve(__dirname, '/all-products.html'),
+        basket: path.resolve(__dirname, '/basket.html'),
       },
     },
   },
