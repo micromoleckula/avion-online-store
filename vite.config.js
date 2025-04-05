3253e8a7d6f6e87a97fcb042f0801742cec4e065
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-04-06 00:08:47 +0300

    test fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        addProduct: path.resolve(__dirname, 'public/add-products.html'),
-        allProducts: path.resolve(__dirname, 'public/all-products.html'),
-        basket: path.resolve(__dirname, 'public/basket.html'),
+        main: 'index.html',
+        addProduct: 'public/add-products.html',
+        allProducts: 'public/all-products.html',
+        basket: 'public/basket.html',
       },
     },
   },
