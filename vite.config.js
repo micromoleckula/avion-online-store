b0e809b567d04c345c22097037f7c6472a29b1e6
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-04-05 23:12:32 +0300

    another fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import html from '@rollup/plugin-html';
 
 export default defineConfig({
+  plugins: [html()],
   build: {
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        addProduct: path.resolve(__dirname, 'add-products.html'),
-        allProducts: path.resolve(__dirname, 'all-products.html'),
-        basket: path.resolve(__dirname, 'basket.html'),
+        addProduct: path.resolve(__dirname, 'public/add-products.html'),
+        allProducts: path.resolve(__dirname, 'public/all-products.html'),
+        basket: path.resolve(__dirname, 'public/basket.html'),
       },
     },
   },
