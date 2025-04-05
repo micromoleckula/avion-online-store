25035fbdbc7edefab98b441e9ef3498372ec06e9
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-04-05 22:40:59 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        signUp: path.resolve(__dirname, 'src/pages/sign-up/sign-up.html'),
         addProduct: path.resolve(__dirname, 'src/pages/add-products/add-products.html'),
         allProducts: path.resolve(__dirname, 'src/pages/all-products/all-products.html'),
         basket: path.resolve(__dirname, 'src/pages/basket/basket.html'),
