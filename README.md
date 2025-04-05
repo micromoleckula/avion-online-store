7f116542f5f8bef0c6702915fe7982e5ba70c430
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-04-05 23:44:39 +0300

    config fix

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Avion - Personal Final Project on JS/TS SoftServe course
 
 There are 5 pages on the site
-1. indexhttps://avion-prokop-dmytro.netlify.app/
+1. index
 2. add new products
 3. all products
 4. shopping basket
--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, '/public/index.html'),
-        addProduct: path.resolve(__dirname, '/public/add-products.html'),
-        allProducts: path.resolve(__dirname, '/public/all-products.html'),
-        basket: path.resolve(__dirname, '/public/basket.html'),
+        main: path.resolve(__dirname, 'index.html'),
+        addProduct: path.resolve(__dirname, 'public/add-products.html'),
+        allProducts: path.resolve(__dirname, 'public/all-products.html'),
+        basket: path.resolve(__dirname, 'public/basket.html'),
       },
     },
   },
